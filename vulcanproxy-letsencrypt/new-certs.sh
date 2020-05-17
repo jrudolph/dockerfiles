bb6602fcb2f642f1b116fdceb321258f5c81ca13
Johannes Rudolph <johannes.rudolph@gmail.com>
2020-05-17 15:42:28 +0200

    fix scripts for latest ubuntu version

--- vulcanproxy-letsencrypt/new-certs.sh
+++ vulcanproxy-letsencrypt/new-certs.sh
@@ -5,7 +5,7 @@ set -e
 
 ETCD_ENDPOINT=${ETCD_ENDPOINT:-172.17.0.1:4001}
 # this hopes that inside the container ifconfig first reports for `eth0`
-CONTAINER_ETH0_IP=`ifconfig eth0| perl -nle 's/dr:(\S+)/print $1/e'|head -n1`
+CONTAINER_ETH0_IP=`/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
 MYIP=${MYIP:-$CONTAINER_ETH0_IP}
 
 # tell the frontend where we are
@@ -13,7 +13,7 @@ curl -X PUT http://$ETCD_ENDPOINT/v2/keys/vulcand/backends/letsencrypt/backend -
 curl -X PUT http://$ETCD_ENDPOINT/v2/keys/vulcand/frontends/letsencrypt/frontend -d 'value={"Type": "http", "BackendId": "letsencrypt", "Route": "PathRegexp(\"/.well-known/.*\")"}'
 curl -X PUT http://$ETCD_ENDPOINT/v2/keys/vulcand/backends/letsencrypt/servers/srv1 -d "value={\"url\":\"http://$MYIP\"}"
 
-certbot certonly --standalone --standalone-supported-challenges http-01 $*
+certbot certonly --standalone --preferred-challenges http-01 $*
 
 curl -X DELETE http://$ETCD_ENDPOINT/v2/keys/vulcand/frontends/letsencrypt?recursive=true
 curl -X DELETE http://$ETCD_ENDPOINT/v2/keys/vulcand/backends/letsencrypt?recursive=true
